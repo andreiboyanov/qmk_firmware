efdec245406c684e680e6599d92b20e3f03b45c1
Andrei Boyanov <andrei.boyanov@haulogy.net>
2021-01-14 14:31:34 +0100

    Remove comments

--- keyboards/kyria/keymaps/andreicho/keymap.c
+++ keyboards/kyria/keymaps/andreicho/keymap.c
@@ -1,18 +1,3 @@
-/* Copyright 2019 Thomas Baart <thomas@splitkb.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include QMK_KEYBOARD_H
 
 enum layers {
@@ -65,81 +50,24 @@ enum layers {
 #define X KC_NO
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/*
- * Base Layer: QWERTY
- *
- * ,-------------------------------------------.                              ,-------------------------------------------.
- * |        |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |        |
- * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |        |   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |        |
- * |        |  GUI |  ALT | CTRL | SHFT |      |                              | SHIFT| CTRL |  ALT |  GUI |      |        |
- * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |        |   Z  |   X  |   C  |   V  |   B  |      |      |  |      |      |   N  |   M  | ,  < | . >  | /  ? |        |
- * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |  ALT | SHFT | Enter| Enter| Tab  |  | Space| Space| Bks  | Del  | ALT  |
- *                        |      |      | NUM  | NAV  | SYM  |  | SYM  | NAV  | NUM  |      |      |
- *                        `----------------------------------'  `----------------------------------'
- */
     [_QWERTY] = LAYOUT(
       X, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    X,
       X, LGUI_A,  LALT_S,  LCTL_D,  LSFT_F,  KC_G,               KC_H,    RSFT_J,  RCTL_K,  RALT_L,  RGUI_SC, X,
       X, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, X, X,   X, X, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, X,
              KC_LALT, KC_LSFT, NUM_ESC, SYM_TAB, NAV_ENT,  NAV_SPC, SYM_SPC, NUM_BSP, KC_DEL,  KC_RALT
     ),
-/*
- * Num Layer
- *
- * ,-------------------------------------------.                              ,-------------------------------------------.
- * |        |      |      |      |      |      |                              |      |      |      |      |      |        |
- * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |                              |      |      |      |      |      |        |
- * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |      |  |      |      |      |      |      |      |      |        |
- * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      |      |      |      |      |  |      |      |      |      |      |
- *                        |      |      |      |      |      |  |      |      |      |      |      |
- *                        `----------------------------------'  `----------------------------------'
- */
     [_NUM] = LAYOUT(
       X, XXXXXXX, KC_F7,   KC_F8,   KC_F9,   KC_F10,              KC_0,    KC_7,    KC_8,    KC_9,    KC_PSCR, X,
       X, KC_LGUI, LALT_F4, LCTL_F5, LSFT_F6, KC_F11,              KC_0,    RSFT_4,  RCTL_5,  RALT_6,  RGUI_IN, X,
       X, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F12, X, X,  X, X, KC_0,    KC_1,    KC_2,    KC_3,    KC_DEL,  X,
               _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______
     ),
-/*
- * Sym Layer
- *
- * ,-------------------------------------------.                              ,-------------------------------------------.
- * |        |      |      |      |      |      |                              |      |      |      |      |      |        |
- * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |                              |      |      |      |      |      |        |
- * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |      |  |      |      |      |      |      |      |      |        |
- * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      |      |      |      |      |  |      |      |      |      |      |
- *                        |      |      |      |      |      |  |      |      |      |      |      |
- *                        `----------------------------------'  `----------------------------------'
- */
     [_SYM] = LAYOUT(
       X, XXXXXXX, KC_AMPR, KC_ASTR, KC_PIPE, KC_COLN,              KC_DQUO,    KC_LPRN, KC_RPRN, KC_EQL,  KC_ASTR, X,
       X, KC_LGUI, KC_DLR,  KC_PERC, KC_CIRC, KC_TILD,              KC_QUOT,    RSFT_LB, RCTL_RB, RALT_MI, RGUI_PL, X,
       X, XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, KC_QUES, X, X,  X, X, KC_GRV,     KC_LCBR, KC_RCBR, KC_UNDS, KC_BSLS, X,
                _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______
     ),
-/*
- * Nav Layer
- *
- * ,-------------------------------------------.                              ,-------------------------------------------.
- * |        |      |      |      |      |      |                              |      |      |      |      |      |        |
- * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |                              |      |      |      |      |      |        |
- * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |      |  |      |      |      |      |      |      |      |        |
- * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      |      |      |      |      |  |      |      |      |      |      |
- *                        |      |      |      |      |      |  |      |      |      |      |      |
- *                        `----------------------------------'  `----------------------------------'
- */
     [_NAV] = LAYOUT(
       X, _______, _______, _______, _______, _______,              KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_PSCR, X,
       X, _______, _______, _______, _______, _______,              KC_PGDN, RSFT_LF, RCTL_DN, RALT_RG, RGUI_IN, X,
